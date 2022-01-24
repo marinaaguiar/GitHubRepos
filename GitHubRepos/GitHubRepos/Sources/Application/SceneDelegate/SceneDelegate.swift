cd4ace8f2d754a760e5e073698fbe75370d915b1
Marina Aguiar <gordinho.marina@gmail.com>
2022-01-24 19:21:46 +0000

    working in progress: setting up the list view controller

--- GitHubRepos/GitHubRepos/Sources/Application/SceneDelegate/SceneDelegate.swift
+++ GitHubRepos/GitHubRepos/Sources/Application/SceneDelegate/SceneDelegate.swift
@@ -11,14 +11,13 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
         
-        let viewController = ListView()
+        let viewController = ListViewController()
         let navigationController = UINavigationController(rootViewController: viewController)
         
         window.rootViewController = navigationController
