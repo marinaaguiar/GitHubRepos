494daf3554d43f5264b20b0c98e2156cd2dc11a7
Marina Aguiar <gordinho.marina@gmail.com>
2022-01-24 13:41:24 +0000

    setting up the project

---
+++ GitHubRepos/GitHubRepos/Sources/Application/Networking/APIService.swift
@@ -0,0 +1,12 @@
+//
+//  APIService.swift
+//  GitHubRepos
+//
+//  Created by Marina Aguiar on 1/24/22.
+//
+
+import Foundation
+
+class APIService {
+    
+}
---
+++ GitHubRepos/GitHubRepos/Sources/Application/Scenes/RepoList/ListView.swift
@@ -0,0 +1,18 @@
+//
+//  ListView.swift
+//  GitHubRepos
+//
+//  Created by Marina Aguiar on 1/24/22.
+//
+
+import UIKit
+
+class ListView: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
